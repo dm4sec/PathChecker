c135c51a54ba8ed7b466243b107c893e8b3db4e3
tangzhushou <ellison.tang@gmail.com>
2020-11-12 12:28:58 +0800

    Last update for closing this project.

--- README.md
+++ README.md
@@ -13,22 +13,22 @@ in the path from *a* to *c*, *b* dominate *c*, that's it!
 ## How to use this tool
 1. Collect all entry nodes of an 
 APK file (or a JAR file).
-2. Collect all target nodes of an 
+2. Collect all resource nodes of an 
 APK file (or a JAR file).
 3. Collect all sanitizers.  
-(all these 3 steps can be done manually or automatically.)
+All these 3 steps can be done manually or automatically. I designed [*CGChecker*](https://github.com/dm4sec/PathChecker/blob/master/src/PathChecker/CGChecker.java) for collecting entries, eg.
+
 
 **Remark 1**:
 When using the *JimpleBasedInterproceduralCFG*, pay special attention to the exception handler in the graph. It may violate the domination and you should check the exception handler deliberately to verify the integrity.  
 **Remark 2**:
-Be cautious not to dumpGraph all the time, or it will crash the application.
+Be cautious not to use dumpGraph all the time, or it will crash the application.
  
 ## TODO
 1. There are quirks when dealing with the \<cinit\> method.
 2. Since I inflate the graph (inline but not context-sensitive), the *b* may not dominate *c* anymore.
-3. $r4 = <com.huawei.fastapp.utils.k: com.huawei.fastapp.commons.adapter.e b> should be splited into AssignStmt and InvokeStmt, however, it doesn't matter.  
-4. **Deal with the exception in the CFG.**
-5. **Shrink nodes in the CFG, remove unrelated branch, etc.**
+3. **Deal with the exception in the CFG.**
+4. **Shrink nodes in the CFG, remove unrelated branch, etc.**
 
 ## Reference
 https://arpith.xyz/2016/05/insertonedge-soot/  
--- src/PathChecker/CGAnalyzer.java
+++ src/PathChecker/CGAnalyzer.java
@@ -54,34 +54,38 @@ public class CGAnalyzer extends SceneTransformer{
             }
         }
 
-        // preserve entries to permSanitizer
-        Set<SootMethod> entries_perm = new HashSet<>();
+        // public static boolean a(java.lang.String r15, java.lang.String r16, long r17, int r19, boolean r20) throws com.huawei.secure.android.common.util.SecurityCommonException {
+        // com.huawei.secure.android.common.util.k
+
+        // preserve entries to unzipSanitizer
+        // not tested
+        Set<SootMethod> entries_path_1 = new HashSet<>();
         {
-            SootMethod permSanitizer = cgHelper.findMethod(
-                    "com.huawei.fastapp.api.permission.DynamicPermission",
-                    "boolean a(java.lang.String,java.lang.String)"
+            SootMethod pathSanitizer_1 = cgHelper.findMethod(
+                    "com.huawei.secure.android.common.util.k",
+                    "boolean a(java.lang.String,java.lang.String,long,int,boolean)"
             );
 
-            if (permSanitizer == null) {
+            if (pathSanitizer_1 == null) {
                 logInfo.warning("Can't find the sanitizer.");
                 return;
             }
 
             this.callstack.clear();
-            this.callstack.push(permSanitizer);
-            this.backwardTrackEntries(cgHelper, entries_perm, permSanitizer, 0);
+            this.callstack.push(pathSanitizer_1);
+            this.backwardTrackEntries(cgHelper, entries_path_1, pathSanitizer_1, 0);
         }
 
-        // preserve js entries to permSanitizer
-        Set<SootMethod> js_entries_perm = new HashSet<>();
+        // preserve js entries to unzipSanitizer
+        Set<SootMethod> js_entries_path_1 = new HashSet<>();
         {
-            for(SootMethod m: entries_perm)
+            for(SootMethod m: entries_path_1)
             {
                 VisibilityAnnotationTag tag = (VisibilityAnnotationTag) m.getTag("VisibilityAnnotationTag");
                 if (tag != null) {
                     for (AnnotationTag annotation : tag.getAnnotations()) {
                         if (annotation.getType().equals("Lcom/taobao/weex/annotation/JSMethod;")) {
-                            js_entries_perm.add(m);
+                            js_entries_path_1.add(m);
                             break;
                         }
                     }
@@ -89,13 +93,82 @@ public class CGAnalyzer extends SceneTransformer{
             }
         }
 
-        // TODO:: which permission is used to protect this entry.
-        Map<SootMethod, List<String>> methodToPermMap = new HashMap<SootMethod, List<String>>();
-
         {
-            System.out.println("Total " + js_entries_perm.size() + " entries protected by permission");
-            for(SootMethod m: js_entries_perm){
+            System.out.println("Total " + js_entries_path_1.size() + " entries protected by zip sanitizer");
+            for(SootMethod m: js_entries_path_1){
                 System.out.println(m.toString());
+            }
+        }
+
+        // preserve entries to permSanitizer
+        Set<SootMethod> entries_perm_0 = new HashSet<>();
+        {
+            SootMethod permSanitizer_0 = cgHelper.findMethod(
+                    "com.huawei.fastapp.api.permission.DynamicPermission",
+                    "boolean a(java.lang.String,java.lang.String)"
+            );
+
+            if (permSanitizer_0 == null) {
+                logInfo.warning("Can't find the sanitizer.");
+                return;
+            }
+
+            this.callstack.clear();
+            this.callstack.push(permSanitizer_0);
+            this.backwardTrackEntries(cgHelper, entries_perm_0, permSanitizer_0, 0);
+        }
+
+        // preserve js entries to permSanitizer
+        Set<SootMethod> js_entries_perm_0 = new HashSet<>();
+        {
+            for(SootMethod m: entries_perm_0)
+            {
+                VisibilityAnnotationTag tag = (VisibilityAnnotationTag) m.getTag("VisibilityAnnotationTag");
+                if (tag != null) {
+                    for (AnnotationTag annotation : tag.getAnnotations()) {
+                        if (annotation.getType().equals("Lcom/taobao/weex/annotation/JSMethod;")) {
+                            js_entries_perm_0.add(m);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // .method private a(Landroid/content/Context;Lcom/taobao/weex/WXSDKInstance;Lcom/huawei/fastapp/api/permission/a;Ljava/lang/String;Z)V
+
+        // preserve entries to permSanitizer_1
+        Set<SootMethod> entries_perm_1 = new HashSet<>();
+        {
+            SootMethod permSanitizer_1 = cgHelper.findMethod(
+                    "com.huawei.fastapp.api.permission.DynamicPermission",
+                    "void a(android.content.Context,com.taobao.weex.WXSDKInstance,com.huawei.fastapp.api.permission.a,java.lang.String,boolean)"
+            );
+
+            if (permSanitizer_1 == null) {
+                logInfo.warning("Can't find the sanitizer.");
+                return;
+            }
+
+            this.callstack.clear();
+            this.callstack.push(permSanitizer_1);
+            this.backwardTrackEntries(cgHelper, entries_perm_1, permSanitizer_1, 0);
+        }
+
+        // preserve js entries to permSanitizer_1
+        Set<SootMethod> js_entries_perm_1 = new HashSet<>();
+        {
+            for(SootMethod m: entries_perm_1)
+            {
+                VisibilityAnnotationTag tag = (VisibilityAnnotationTag) m.getTag("VisibilityAnnotationTag");
+                if (tag != null) {
+                    for (AnnotationTag annotation : tag.getAnnotations()) {
+                        if (annotation.getType().equals("Lcom/taobao/weex/annotation/JSMethod;")) {
+                            js_entries_perm_1.add(m);
+                            break;
+                        }
+                    }
+                }
             }
         }
 
@@ -118,14 +191,57 @@ public class CGAnalyzer extends SceneTransformer{
 
         Set<SootMethod> analysisResult = new HashSet<>();
         {
+            System.out.println("Total " + js_entries.size() + " entries");
+            System.out.println("Total " + js_entries_path.size() + " entries are protected by path sanitizer");
+            System.out.println("Total " + js_entries_perm_0.size() + " entries are protected by perm0");
+            System.out.println("Total " + js_entries_perm_1.size() + " entries are protected by perm1");
+
+            analysisResult.clear();
+            analysisResult.addAll(js_entries_perm_0);
+            analysisResult.addAll(js_entries_perm_1);
+
+            {
+                System.out.println("Total " + analysisResult.size() + " entries protected by permission");
+                for(SootMethod m: analysisResult){
+                    System.out.println(m.toString());
+                }
+            }
+
             analysisResult.clear();
             analysisResult.addAll(js_entries);
             analysisResult.removeAll(js_entries_path);
-            analysisResult.removeAll(js_entries_perm);
+            analysisResult.removeAll(js_entries_perm_0);
+            analysisResult.removeAll(js_entries_perm_1);
 
             System.out.println("Total " + analysisResult.size() + " un-protected entries");
             for(SootMethod m: analysisResult){
                 System.out.println(m.toString());
+            }
+        }
+        // latex format output
+        {
+            List<SootMethod> js_entries_sorted = new ArrayList<>(js_entries);
+            Collections.sort(js_entries_sorted,
+                    new Comparator<SootMethod>()
+                    {
+                        public int compare(SootMethod m1, SootMethod m2)
+                        {
+                            return m1.toString().compareTo(m2.toString());
+                        }
+                    });
+
+            System.out.println("latex format");
+            int i = 1;
+            for(SootMethod m: js_entries_sorted) {
+                String line = String.format("%d & %s & %s & %s & %s \\\\\\hline",
+                        i,
+                        m.toString(),
+                        js_entries_path.contains(m) ? "$\\blacksquare$" : " ",
+                        js_entries_perm_0.contains(m) ? "$\\blacksquare$" : " ",
+                        js_entries_perm_1.contains(m) ? "$\\blacksquare$" : " "
+                );
+                System.out.println(line);
+                i ++;
             }
         }
 
@@ -134,10 +250,6 @@ public class CGAnalyzer extends SceneTransformer{
     }
 
 
-    private void forwardTrackPerms()
-    {
-
-    }
     /*
     About Annotations:
 
@@ -196,6 +308,10 @@ public class CGAnalyzer extends SceneTransformer{
             }
         }else
         {
+//            for(SootMethod cs: this.callstack)
+//            {
+//                System.out.println(cs);
+//            }
             entries.add(m);
         }
         this.callstack.pop();
